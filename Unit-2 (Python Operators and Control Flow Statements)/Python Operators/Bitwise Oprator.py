1f22eba9731969f4096eb7a1bbfdc08852202c07
Avishkar Kumbhar <kumbharavishkar7@gmail.com>
2023-05-17 21:11:31 +0530

    daily update

--- Unit-2 (Python Operators and Control Flow Statements)/Python Operators/Bitwise Oprator.py
+++ Unit-2 (Python Operators and Control Flow Statements)/Python Operators/Bitwise Oprator.py
@@ -36,6 +36,19 @@
     7 | 14 =>1 1 1 1 = >15
 
 
+    Bitwise XOR (^)
+
+    7^2
+    7 => 0 1 1 1
+    2 => 0 0 1 0
+    -------------
+    7^2=>0 1 0 1 =>5
+
+    Bitwise NOT (~)
+    ~13 => 1 1 0 1
+    ---------------
+     2=> 0 0 1 0
+
 
     Bitwise (<<) left shift  =>
     7<<2
@@ -51,12 +64,15 @@
     left shif =  0 0 0 0 0 0 0 1
     
     7>>2 = 1
+
 '''
 
-print(" Bitwise & :  7 & 14 =>", 7&14);
-print(" Bitwise | :  7 | 14 =>", 7|14);
-print(" Bitwise << : 7<<2 =>", 7<<2);
-print(" Bitwise >> : 7>>2 =>", 7>>2);
+print(" Bitwise & (AND) :  7 & 14 =>", 7&14) #6
+print(" Bitwise | (OR)  :  7 | 14 =>", 7|14) #15
+print(" Bitwise ^ (XOR) :  7 ^ 2 =>",7^2) #5
+print(" Bitwise ~ (NOT) :  ~13 =>",~13) # 2
+print(" Bitwise <<(Left Shift): 7<<2 =>", 7<<2) #28
+print(" Bitwise >>(Right Shift): 7>>2 =>", 7>>2) #1
 
 
 
